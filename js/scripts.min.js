8bda38ba39adabe9b92f62e66c56d68e1ac22b8b
edwardlie <emailedwardlie@gmail.com>
2025-04-07 09:37:32 -0400

    Update header text

--- js/scripts.min.js
+++ js/scripts.min.js
@@ -1,10 +1,7 @@
 /*!
-    Title: Dev Portfolio Template
-    Version: 1.2.2
-    Last Change: 03/25/2020
-    Author: Ryan Fitzgerald
-    Repo: https://github.com/RyanFitzgerald/devportfolio-template
-    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
+    Title: Edward Lie
+    Version: 1.2.3
+    Author: Edward Lie
 
     Description: This file contains all the scripts associated with the single-page
     portfolio website.
